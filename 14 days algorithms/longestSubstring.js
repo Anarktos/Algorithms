e58bf2cc5b8315dcab33eb35c1871bf287995992
Anarktos <camilowhite7@gmail.com>
2022-05-16 11:53:19 -0500

    fix

--- 14 days algorithms/longestSubstring.js
+++ 14 days algorithms/longestSubstring.js
@@ -1,13 +1,23 @@
 const lenghtOfLongestSubstring = (s) => {
-    let sub = [];
-    for (let i = 0; i < s.length; i++){
-        if (sub.indexOf(s[i]) !== -1) {
-            sub.splice(0, sub.indexOf(s[i]) + 1);
+    let a = 0;
+    let b = 0;
+    let max = 0;
+    let set = new Set();
+
+    while (b < s.length) {
+        if (!set.has(s[b])) {
+            set.add(s[b]);
+            b++;
+            max = Math.max(max, b - a);
+        } else {
+            set.delete(s[a]);
+            a++;
         }
-        sub.push(s[i]);
     }
-    return sub.length;
+
+    return max;
+
 }
 
-console.log(lenghtOfLongestSubstring('abcabcbb'));
+console.log(lenghtOfLongestSubstring("pwwkew"));
 
